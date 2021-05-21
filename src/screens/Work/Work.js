27a28ba82befc33f1a4f589edd5403d598c4930d
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-05-20 20:24:24 -0700

    added more technical details and credit on work pieces.

--- src/screens/Work/Work.js
+++ src/screens/Work/Work.js
@@ -6,12 +6,12 @@ export default function Work() {
     <div id='work-container'>
       <div className='work-desc'>
         <h3>Pokemon Card Database</h3>
-        <p>"Pokemon Card Database" is an app that utilizes information from the Pokemon TCG Database to render information regarding pokemon cards.  This information includes the stats of the cards as well as links for purchasing and pricing.</p>
+        <p>"Pokemon Card Database" is an app that utilizes information from the Pokemon TCG API to render information regarding pokemon cards.  This information includes the stats of the cards as well as links for purchasing and pricing.</p>
       </div>
       <a href='https://pages.git.generalassemb.ly/craignavarro/PokeCard-Info/' target='_blank' rel='noreferrer noopener' className='work-link'>
         <img className='work-img' src='https://github.com/foremanlb/Portfolio/blob/main/src/assets/Screen%20Shot%202021-05-20%20at%2012.40.09%20PM.png?raw=true' alt='Pokemon cards' />
       </a>
-      <a href='https://pages.git.generalassemb.ly/craignavarro/PokeCard-Info/' target='_blank' rel='noreferrer noopener' className='work-link'>
+      <a href='https://606722d96cf0de00089700de--unruffled-northcutt-061161.netlify.app/' target='_blank' rel='noreferrer noopener' className='work-link'>
         <img className='work-img' src='https://github.com/foremanlb/Portfolio/blob/main/src/assets/Screen%20Shot%202021-05-20%20at%2012.40.38%20PM.png?raw=true' alt='comic release' />
       </a>
       <div className='work-desc'>
@@ -20,17 +20,17 @@ export default function Work() {
       </div>
       <div className='work-desc'>
         <h3>Dev it Forward</h3>
-        <p>"Dev it Forward" is a concept site to bring together users and tutors from the development world.  Users can view different tutors to find someone with the skills they are looking to learn.  Tutors can create their profile to display their expertises to those looking to learn.</p>
+        <p>"Dev it Forward" is a concept site to bring together users and tutors from the development world.  This was a joint project between myself and three others: Francis Nino, Will Benedict, and Ikechukwu Ekedede.  Utilizing a MERN Stack, Users can view different tutors to find someone with the skills they are looking to learn.  Tutors can create their profile to display their expertises to those looking to learn.</p>
       </div>
-      <a href='https://pages.git.generalassemb.ly/craignavarro/PokeCard-Info/' target='_blank' rel='noreferrer noopener' className='work-link'>
+      <a href='https://gracious-hodgkin-2b0838.netlify.app/Landing' target='_blank' rel='noreferrer noopener' className='work-link'>
         <img className='work-img' src='https://github.com/foremanlb/Portfolio/blob/main/src/assets/Screen%20Shot%202021-05-20%20at%2012.41.24%20PM.png?raw=true' alt='dev it forward' />
       </a>
-      <a href='https://pages.git.generalassemb.ly/craignavarro/PokeCard-Info/' target='_blank' rel='noreferrer noopener' className='work-link'>
+      <a href='https://gifted-nobel-528bae.netlify.app/' target='_blank' rel='noreferrer noopener' className='work-link'>
         <img className='work-img' src='https://github.com/foremanlb/Portfolio/blob/main/src/assets/Screen%20Shot%202021-05-20%20at%2012.42.35%20PM.png?raw=true' alt='camping companions' />
       </a>
       <div className='work-desc'>
         <h3>Camping Companions</h3>
-        <p>"Camping Companions" is designed to allow people to interact with each other in order to plan camping trips with new friends.</p>
+        <p>"Camping Companions" is designed to allow people to interact with each other in order to plan camping trips with new friends.  This app was made using Ruby on rails, rendering through React.</p>
       </div>
     </div>
   )
